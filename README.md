07d79f7e1d494932a704ad439fd3e4942891a70f
darvin <darvintojo@gmail.com>
2025-01-16 07:58:29 -0500

    Scala loops examples

---
+++ README.md
@@ -0,0 +1,9 @@
+## Scala | Loops(while, do..while, for, nested loops)
+
+
+
+### Technologies
+
+- Java 17
+- Sbt 1.10.17
+- Scala 3.3.4
---
+++ src/main/scala/main.scala
@@ -0,0 +1,7 @@
+@main
+def main(): Unit =
+  println("--------------------------------------------------------------------------------------")
+  println("Hello world! This is an example for Scala | Loops(while, do..while, for, nested loops)")
+
+
+
